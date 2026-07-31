7239ada3a1996e6f094fbc8245e7e794f3cd808d
OrtelanGabriel1 <gabrielortelan7@gmail.com>
2026-07-31 11:04:45 -0300

    rascunho do database.js e .gitignore criado

---
+++ backend/src/config/database.js
@@ -0,0 +1,7 @@
+// 1. importar dotenv e "carregar" o .env
+// 2. importar o mysql2 (versão promise)
+
+// 3. criar o pool de conexões, passando host, user, password, database, port
+//    (usando process.env.X pra cada valor)
+
+// 4. exportar o pool pra ser usado em outros arquivos
