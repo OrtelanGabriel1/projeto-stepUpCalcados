ad1ff0057f047d9b929fdc24098b9d74f54ef772
OrtelanGabriel1 <gabrielortelan7@gmail.com>
2026-08-07 09:02:26 -0300

    Métodos get criados

--- backend/src/services/produtoService.js
+++ backend/src/services/produtoService.js
@@ -2,12 +2,31 @@ const ProdutoRepository = require('../repositories/produtoRepository')
 
 class ProdutoService{
 
+async buscarTodosProduto(){
+    const produtos = ProdutoRepository.buscarTodos()
+    return {
+        sucesso: true,
+        dados: produtos
+    }
+}
+
+async buscarProdutoPorId(id){
+    if (id === undefined || isNaN(id)){
+        throw {status: 400, mensagem:"id é obrigatório e deve ser um número"}
+    }
+    const produto = await ProdutoRepository.buscarPorId(id)
+    if (!produto){
+        throw {status: 404, mensagem:"Produto não encontrado"}
+    }
+
+    return{
+        sucesso: true,
+        dados: produto
+    }
+}
+
 async criarProduto(dados){
     let {nome, descricao, preco_venda, preco_custo, tamanho, cor, genero} = dados
-
-    if (preco_venda <= 0 || preco_custo <= 0) {
-            throw { status: 400, mensagem: "Preço deve ser um número positivo" };
-        }
 
     if (!nome || !cor || preco_venda === undefined || preco_custo === undefined || tamanho === undefined){
         throw { status: 400, mensagem: "Nome, cor, preço de venda, preço de custo e tamanho são obrigatórios"};
@@ -16,6 +35,9 @@ async criarProduto(dados){
     if (isNaN(preco_venda) || isNaN(preco_custo) || isNaN(tamanho)){
         throw {status: 400, mensagem:"Preço de venda, preço de custo e tamanho devem ser números"};
     }
+    if (preco_venda <= 0 || preco_custo <= 0) {
+            throw { status: 400, mensagem: "Preço deve ser um número positivo" };
+        }
 
     const novoProduto = {
         nome, descricao, preco_venda, preco_custo, tamanho, cor, genero 
@@ -29,6 +51,8 @@ async criarProduto(dados){
             id
         };
 }
+
+
 
 }
 
